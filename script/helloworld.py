a475ef0e687a80a27d78bb4219b8b85c474a6e31
yusufsn <yusufsulistyonugroho@gmail.com>
2017-12-13 13:45:11 +0900

    added the number of loop in helloworld.py file

--- script/helloworld.py
+++ script/helloworld.py
@@ -1,6 +1,7 @@
 #run on python version 2.*
 
-x = raw_input(str('Input your text = '))
+x = raw_input('Input your text = ')
+y = int(raw_input('Input the number of loop = '))
 
-for i in range(0,10):
+for i in range(0,y):
 	print i+1, x
