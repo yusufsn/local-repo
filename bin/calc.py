a3622a0148ef25516734c3a8c2581e8a896440d6
yusufsn <yusufsulistyonugroho@gmail.com>
2018-01-03 11:50:15 +0900

    added multiplication and division in calc.py, also modified helloworld to merge

--- bin/calc.py
+++ bin/calc.py
@@ -6,8 +6,18 @@ def substract(x,y):
 	z = x-y
 	return z
 
+def multiply(x,y):
+	z = x*y
+	return z
+
+def div(x,y):
+	z = x/y
+	return z
+
 a = float(input("Input your first number = "))
 b = float(input("Input your second number = "))
 
 print ("The result of addition of {} and {} = {}".format(a,b,add(a,b)))
 print ("The result of substraction of {} and {} = {}".format(a,b,substract(a,b)))
+print ("The result of multiplication of {} and {} = {}".format(a,b,multiply(a,b)))
+print ("The result of division of {} and {} = {}".format(a,b,div(a,b)))
--- script/helloworld.py
+++ script/helloworld.py
@@ -4,6 +4,9 @@ x = raw_input('Input your text = ')
 y = int(raw_input('Input the number of loop = '))
 
 #loop code
+<<<<<<< HEAD
 
+=======
+>>>>>>> 8d9b0e0... added comment before the loop code in helloworld.py file
 for i in range(0,y):
 	print i+1,x
