51d2ad72220e5508163e7e3fbbebe8cc6f606742
yusufsn <yusufsulistyonugroho@gmail.com>
2018-02-05 16:56:32 +0900

    added 2 new files: mergingmultipledata.py and data.csv --> contains of code to merge multiple duplicated data and write into csv file

---
+++ script/mergingmultipledata.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os, csv
+import numpy as np
+
+userhome = os.path.expanduser('~')
+filename = userhome + r'/local-repo/script/data.csv' 
+
+data = pd.DataFrame(np.random.randint(low=0, high=10, size=(20,5)),
+					columns=['a', 'b', 'c', 'd', 'e'])
+
+print('data frame :')
+print(data)
+
+#write into csv
+data.to_csv(filename)
+
+#drop duplicate data
+dd = data.drop_duplicates(['d', 'e'])
+print('Drop duplicate data based on column d and e:')
+print(dd)
