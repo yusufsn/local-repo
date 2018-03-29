fcf2db362771e34dd06378449ae5a61810769872
yusufsn <yusufsulistyonugroho@gmail.com>
2018-03-29 14:22:37 +0900

    modified helloworld.py by deleting tabular lines, adding comment and completion information to print out

--- script/helloworld.py
+++ script/helloworld.py
@@ -3,8 +3,9 @@
 x = raw_input('Input your text = ')
 y = int(raw_input('Input the number of loop = '))
 
-	
 #loop code
-
+#this is just for trying the loop
 for i in range(0,y):
 	print i+1,x
+
+print "\nthe loop is complete"
