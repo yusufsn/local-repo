e1a302f1f1b1d5272885ec87c1e661b3c9faa8c8
yusufsn <yusufsulistyonugroho@gmail.com>
2018-06-20 16:38:33 +0900

    updated diff_test.py

--- script/diff_test.py
+++ script/diff_test.py
@@ -1,8 +1,8 @@
-for i in range(0,1):
-	f=open(files[i],"r")
-	header=next(f).replace("\n","")
-
 for file in files:
 	#for getting file
 	f=open(file,"r")
 	next(f)
+	
+for i in range(0,1):
+	f=open(files[i],"r")
+	header=next(f).replace("\n","")
